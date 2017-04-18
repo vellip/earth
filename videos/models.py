dff8f688522ff5009106d0614df813a1d2e0abfe
Philipp <vellerphilipp@gmail.com>
2017-04-18 20:24:16 +0200

    add vimeo api and handling

--- videos/models.py
+++ videos/models.py
@@ -1,5 +1,9 @@
+import vimeo
 from django.db import models
 from random import randint
+
+from earth_django.settings import VIMEO_CLIENT_ID
+from earth_django.settings import VIMEO_SECRET
 
 
 class VideosManager(models.Manager):
@@ -11,6 +15,17 @@ class VideosManager(models.Manager):
 
 class Video(models.Model):
     objects = VideosManager()
-    video_id = models.CharField(max_length=20)
+    video_id = models.CharField(max_length=20, unique=True)
+    name = models.CharField(max_length=250, blank=True)
+    description = models.CharField(max_length=2000, blank=True)
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        v = vimeo.VimeoClient(
+            key=VIMEO_CLIENT_ID,
+            secret=VIMEO_SECRET)
 
+        token = v.load_client_credentials()
+        video = v.get('/videos/{}'.format(self.video_id)).json()
+        self.name = video['name']
+        self.description = video['description']
--- videos/views.py
+++ videos/views.py
@@ -7,4 +7,5 @@ class RandomVideoView(generic.DetailView):
     template_name = 'random_video.html'
 
     def get_object(self, queryset=None):
+        Video.objects.create(video_id='136938394')
         return Video.objects.random()
