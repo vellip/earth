30b8e9fa804ffc146304f3305cfdf6875a3312d1
Philipp Veller <mail@vellip.de>
2021-01-29 18:17:39 +0100

    reformat server/index.js

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const next = require('next')
 const express = require('express')
 const { ApolloServer } = require('apollo-server-express')
@@ -8,22 +9,24 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-Promise.all([app.prepare(), adminRouter]).then((values) => {
-  const expressApp = express()
-  const apolloServer = new ApolloServer({
-    schema: schema,
-    cors: true,
-    playground: true,
-    introspection: true,
-    tracing: true,
-    path: '/',
-  })
-  apolloServer.applyMiddleware({ app: expressApp })
-  expressApp.use('/admin', values[1])
-  expressApp.get('*', (req, res) => handle(req, res))
+Promise.all([app.prepare(), adminRouter])
+  .then((values) => {
+    const expressApp = express()
+    const apolloServer = new ApolloServer({
+      schema: schema,
+      cors: true,
+      playground: true,
+      introspection: true,
+      tracing: true,
+      path: '/',
+    })
+    apolloServer.applyMiddleware({ app: expressApp })
+    expressApp.use('/admin', values[1])
+    expressApp.get('*', (req, res) => handle(req, res))
 
-  expressApp.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    expressApp.listen(3000, (err) => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
   })
-})
+  .catch((e) => console.error(e))
