898e4916dd86bd85715a9111871f6d33d0be28f3
Philipp Veller <mail@vellip.de>
2021-01-29 18:16:31 +0100

    add graphql client and dotenv for graph uri

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export const graphqlClient = new ApolloClient({
+  uri: `${process.env.GRAPH_URI}`,
+  ssrMode: typeof window === 'undefined',
+  cache: new InMemoryCache(),
+})
+
+function Earth({ Component, pageProps }) {
+  return (
+    <ApolloProvider client={graphqlClient}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
-export default MyApp
+export default Earth
